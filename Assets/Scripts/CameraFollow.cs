2552aa72c003624d1f673582db0e54c8f1d063fc
AVillegas <alessandrovill2@gmail.com>
2026-03-12 14:08:08 -0500

    added base for player and camera scripts

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public Transform target;
+    public Vector3 offset;
+    
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = target.position + offset; 
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerMovement3D : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Rigidbody rb; 
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontal,0f,vertical);
+
+        rb.linearVelocity = movement * moveSpeed;
+
+    }
+}
